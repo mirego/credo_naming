335f9aec483eec325ca4d3a0f14adf10aebd3e11
Rémi Prévost <remi@exomel.com>
2019-04-19 09:30:44 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,10 +1,18 @@
 defmodule CredoFilenameConsistency.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
+      version: @version,
       app: :credo_filename_consistency,
-      version: "0.1.0",
+      name: "CredoFilenameConsistency",
+      description: "A check to ensure filename consistency across an Elixir project.",
+      source_url: "https://github.com/mirego/credo_filename_consistency",
+      homepage_url: "https://github.com/mirego/credo_filename_consistency",
+      docs: [extras: ["README.md"], main: "readme", source_ref: "v#{@version}", source_url: "https://github.com/mirego/credo_filename_consistency"]
+      package: package(),
       elixir: "~> 1.8",
       start_permanent: false,
       deps: deps()
@@ -21,5 +29,15 @@ defmodule CredoFilenameConsistency.MixProject do
     [
       {:credo, "~> 1.0"}
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Rémi Prévost"],
+      licenses: ["BSD-3"],
+      links: %{
+        "GitHub" => "https://github.com/mirego/credo_filename_consistency"
+      }
+    }
   end
 end
